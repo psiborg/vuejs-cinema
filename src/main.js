2985dab05b80fc7ed774dff8a0d7a3411e472329
Jim Ing <jim.ing@gmail.com>
2019-03-14 17:55:47 -0400

    Section 15, Lecture 86

--- src/main.js
+++ src/main.js
@@ -11,7 +11,14 @@ console.log('Hello World');
 */
 import Vue from 'vue';
 import './style.scss';
-import genres from './util/genres';
+
+//import genres from './util/genres';
+
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
+
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
 
 new Vue({
 	el: '#app',
@@ -22,7 +29,8 @@ new Vue({
 */
 	data: {
 		genre: [],
-		time: []
+		time: [],
+		movies: []
 	},
 	methods: {
 		checkFilter(category, title, checked) {
@@ -39,77 +47,14 @@ new Vue({
 		}
 	},
 	components: {
-		'movie-list': {
-			template: `
-<div id="movie-list">
-	<div class="movie" v-for="movie in filteredMovies">{{ movie.title }}</div>
-</div>`,
-			data: function() {
-				return {
-					movies: [
-						{ title: 'Pulp Fiction', genre: genres.CRIME },
-						{ title: 'Home Alone', genre: genres.COMEDY },
-						{ title: 'Austin Powers', genre: genres.COMEDY }
-					]
-				};
-			},
-			props: [ 'genre', 'time' ],
-			methods: {
-				moviePassesGenreFilter(movie) {
-					if (!this.genre.length) {
-						return true;
-					}
-					else {
-						return this.genre.find(genre => movie.genre === genre);
-					}
-				}
-			},
-			computed: {
-				filteredMovies() {
-					return this.movies.filter(this.moviePassesGenreFilter);
-				}
-			}
-		},
-		'movie-filter': {
-			data: function() {
-				return {
-					genres
-				};
-			},
-			template: `
-<div id="movie-filter">
-	<h2>Filter Results</h2>
-	<div class="filter-group">
-		<check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
-	</div>
-</div>`,
-			methods: {
-				checkFilter(category, title, checked) {
-					//console.log('checkFilter');
-					this.$emit('check-filter', category, title, checked);
-				}
-			},
-			components: {
-				'check-filter': {
-					data() {
-						return {
-							checked: false
-						}
-					},
-					props: [ 'title' ],
-					template: `
-<div v-bind:class="{ 'check-filter': true, active: checked }" v-on:click="checkFilter">
-	<span class="checkbox"></span>
-	<span class="check-filter-title">{{ title }}</span>
-</div>`,
-					methods: {
-						checkFilter() {
-							this.checked = !this.checked;
-							this.$emit('check-filter', 'genre', this.title, this.checked);
-						}
-					}
-				}
-			}
-		}
+		MovieList,
+		MovieFilter
+	},
+	created() {
+		//console.log(this.$http);
+		this.$http.get('/api').then(response => {
+			console.log(response.data);
+			this.movies = response.data;
+		});
 	}
 });
